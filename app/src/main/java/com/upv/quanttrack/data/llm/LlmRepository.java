c711219454d7840576fc6748b5ef3bb38be9e6bf
mselarm <mselarm@teleco.upv.es>
2026-07-17 09:30:10 +0200

    Update API key in LlmRepository.java

--- app/src/main/java/com/upv/quanttrack/data/llm/LlmRepository.java
+++ app/src/main/java/com/upv/quanttrack/data/llm/LlmRepository.java
@@ -8,7 +8,7 @@ import retrofit2.Response;
 public class LlmRepository {
 
     // INYECTA TU CLAVE AQUÍ
-    private static final String API_KEY = "AIzaSyAKUf5siAS94bio2UfF__nSPqkXZORUxvQ";
+    private static final String API_KEY = "Tu Clave de gemini";
     private LlmApi api;
 
     // Puente de comunicación asíncrona para la interfaz
